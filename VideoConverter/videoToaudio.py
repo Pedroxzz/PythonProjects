184121ad0f570243f65a84cf8b9869007f73aaa5
João Pedro <joaopedroicm2117@gmail.com>
2022-06-22 19:21:04 -0300

    Primeira versão do conversor de video

---
+++ VideoConverter/videoToaudio.py
@@ -0,0 +1,10 @@
+import moviepy.editor
+
+# Carrega o arquivo/URL do video
+video = moviepy.editor.VideoFileClip("opa.mp4")
+
+# Extrai o áudio do vídeo
+audio_data = video.audio
+
+# Salva o arquivo de áudio extraido
+audio_data.write_audiofile("audio.mp4")
